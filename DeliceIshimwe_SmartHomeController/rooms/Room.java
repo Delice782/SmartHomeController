36e52d77a85c15008ac1add80274d9e1c992e911
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2025-11-27 01:05:19 +0000

    Update Room.java

--- DeliceIshimwe_SmartHomeController/rooms/Room.java
+++ DeliceIshimwe_SmartHomeController/rooms/Room.java
@@ -1,4 +1,4 @@
-         
+          
 package rooms;
 
 import java.util.ArrayList;
@@ -79,3 +79,4 @@ public class Room {
         return "Room: " + name + " with " + devices.size() + " devices";
     }
 }
+
