38eb96c221b47c0b5de095a579e54506cfad7b4d
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2026-03-20 21:39:02 +0000

    Update SmartHomeController.java

--- DeliceIshimwe_SmartHomeController/controller/SmartHomeController.java
+++ DeliceIshimwe_SmartHomeController/controller/SmartHomeController.java
@@ -1,6 +1,6 @@
                                                                                                             
 package controller;   
-  
+   
 import devices.Device; 
 import observers.ActionLogger;
 import observers.Subject; 
