c96fa46d44c3e343dd46ad5d67b043b4885afda2
Delice Ishimwe <154625118+Delice782@users.noreply.github.com>
2026-04-27 23:57:35 +0000

    Update Device.java

--- DeliceIshimwe_SmartHomeController/devices/Device.java
+++ DeliceIshimwe_SmartHomeController/devices/Device.java
@@ -3,7 +3,7 @@ package devices;
 /**
  * Device interface: All smart home devices implement this interface
  */
-public interface Device {
+public interface Device { 
     /**
      * turnOn method: Turn the device on
      */
